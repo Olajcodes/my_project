f1a2b1df2c0dbb4618cf41afc7fd81c84fdaaf9e
Olajcodes <olajideabioye01@gmail.com>
2025-08-28 09:55:45 +0100

    Task 2 pending

--- calculator.py
+++ calculator.py
@@ -39,30 +39,58 @@ print("6. Exponential")
 print("0. Exit")
 
 
-while True:
-
-    #  Take input from the user for operation choice
-    choice = input("Enter choice (1/2/3/4): ")
-
-    #  Get two numbers as input from the user
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
-
+while True: 
+    
+    try:
+        choice = input("Enter choice (1/2/3/4/5/6/0): ")
     #  Perform calculations based on user's choice
-    if choice == "1":
-        print("Result:", add(num1, num2))
-    elif choice == "2":
-        print("Result:", subtract(num1, num2))
-    elif choice == "3":
-        print("Result:", multiply(num1, num2))
-    elif choice == "4":
-        print("Result:", divide(num1, num2))
-    elif choice == "5":
-        print(f"Result for square of {num1}: {square_root(num1)}")
-        print(f"Result for square of {num2}: {square_root(num2)}")
-    elif choice == "6":
-        print("Result:", exponent(num1, num2))
-    elif choice == "0":
-        break
-    else:
-        print("Invalid choice!")
+        if choice == "1":
+            #  Get two numbers as input from the user
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result:", add(num1, num2))
+            
+        elif choice == "2":
+            #  Get two numbers as input from the user
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result:", subtract(num1, num2))
+            
+        elif choice == "3":
+            #  Get two numbers as input from the user
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result:", multiply(num1, num2))
+            
+        elif choice == "4":
+            #  Get two numbers as input from the user
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("Result:", divide(num1, num2))
+            
+        elif choice == "5":
+            #  Get two numbers as input from the user
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print(f"Result for square of {num1}: {square_root(num1)}")
+            print(f"Result for square of {num2}: {square_root(num2)}")
+            
+        elif choice == "6":
+            #  Get two numbers as input from the user
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+            print("The Result for the operation is:", exponent(num1, num2))
+            
+        elif choice == "0":
+            print("\nExiting program. Thanks for your time!\n")
+            break
+        else:
+            print("Invalid choice!")
+
+    except ValueError:
+        print("You entered wrong value. Only numbers accepted.")
+        
+    except KeyboardInterrupt:
+        print("\nThe program got aborted due to keyboard Interrupt!\n")
+        
+    
